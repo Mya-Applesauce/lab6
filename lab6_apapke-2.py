69e7bc6bcbdeee5583156a17063a11d3878b55c6
Ari Papke <myaapplesauce42@gmail.com>
2026-02-22 12:53:40 -0500

    Add files via upload

--- lab6_apapke-2.py
+++ lab6_apapke-2.py
@@ -1,6 +1,6 @@
 shopping_cart = {}
 
-print(f"Hail and well met! Welcome to mine infinite store! We hath ev'rything thy heart desireth, and many things it doth not! Simply entere whatsoever thou wisheth, and it shalt be provided. Thou mayest also entere ""view"" to gaze upon thine cart at any time. Entere ""checkout"" when thou art finished.")
+print(f"Hail and well met! Welcome to mine infinite store! We hath ev'rything thine heart desireth, and many things it doth not! (for a most reasonable price of $9.99 per item) Simply entere whatsoever thou wisheth, and it shalt be provided. Thou mayest also entere ""view"" to gaze upon thy cart at any time. Entere ""checkout"" when thou art finished.")
 
 active = True
 while active:
@@ -8,20 +8,21 @@ while active:
     if (item == "checkout") or (item == "Checkout"):
         active = False
     elif (item == "View") or (item == "view"):
-        print(f"++THINE CART++")
+        print(f"++THY CART++")
         for thing in shopping_cart:
             print(f"{thing}: {shopping_cart[thing]}")
-        print(f"++++++++++++++")
+        print(f"++++++++++++")
     else:
         quantity = int(input(f"How many?"))
         if item not in shopping_cart:
             shopping_cart[item] = quantity
         else:
             shopping_cart[item] += quantity
-        print(f"*{shopping_cart[item]} {item} hath been added to thine cart.")
+        print(f"*{shopping_cart[item]} {item} hath been added to thy cart.")
 
-print("++THINE FINAL CART++")
+print("++THY FINAL CART++")
 for thing in shopping_cart:
             print(f"{thing}: {shopping_cart[thing]}")
-print("++++++++++++++++++++\n")
+print("++++++++++++++++++\n")
+
 print("I thanketh thee for thy patronage!")

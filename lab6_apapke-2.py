86afcc806a1740213d7741c2b22aad48e1e3b6dc
Ari Papke <myaapplesauce42@gmail.com>
2026-02-22 11:58:20 -0500

    Add files via upload

--- lab6_apapke-2.py
+++ lab6_apapke-2.py
@@ -1,7 +1,4 @@
-shopping_cart = {
-
-}
-
+shopping_cart = {}
 
 print(f"Welcome!")
 
@@ -11,8 +8,15 @@ while active:
     if (item == "checkout") or (item == "Checkout"):
         active = False
     elif (item == "View") or (item == "view"):
-        print(f"++YOUR CART++\n")
+        print(f"++YOUR CART++")
+        for thing in shopping_cart:
+            print(f"{thing} {shopping_cart[thing]}")
     else:
-        quantity = input(f"How many?")
-        if item in shopping_cart:
-            print("")
+        quantity = int(input(f"How many?"))
+        if item not in shopping_cart:
+            shopping_cart[item] = quantity
+        else:
+            shopping_cart[item] += quantity
+        print(f"{item} {shopping_cart[item]}")
+for thing in shopping_cart:
+            print(f"{thing} {shopping_cart[thing]}")
